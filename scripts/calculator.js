4844b5b6cdbc20a9bb8b771409bd5ab25fe9c766
Dabira <taiwoadejumo1234@gmail.com>
2026-07-12 18:01:23 +0100

    Move calculator scripts to calculator.js and Add commas to long outputs

---
+++ scripts/calculator.js
@@ -0,0 +1,37 @@
+ function addElement(elem){
+        document.getElementById("js-screen-input").value += elem;
+      }
+      function calculate(){
+        let expression = document.getElementById('js-screen-input').value;
+        expression = expression.replace(/&#215;|×/g,'*');
+        expression = expression.replace(/&#247;|÷/g,'/');
+        try{
+          // const result = math.evaluate(expression);
+          const result = math.evaluate(expression);
+          let resultValue = result;
+          if (String(resultValue).length > 13){
+            resultValue = resultValue.toExponential(5);
+          }
+          document.getElementById("js-screen-result").innerText = addCommas(resultValue);
+        } catch (error){
+          document.getElementById("js-screen-result").innerText = error.message;
+        }
+         
+      }
+
+      function addCommas(number){
+        let [intergerPart, decimalPart] = number.toString().split(".");
+        intergerPart = intergerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+        return decimalPart?`${intergerPart}.${decimalPart}`: intergerPart;
+      }
+
+      function del(){
+        let input = document.getElementById('js-screen-input');
+        input.value= input.value.substr(0, input.value.length - 1);
+      }
+
+      function delAll(){
+        document.getElementById('js-screen-input').value = '';
+        document.getElementById('js-screen-result').innerText = '';
+      }
